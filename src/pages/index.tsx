3a0fd7092c041b84681618bc1a9222411a78bc33
naretaki <naretaki@amazon.com>
2025-05-31 13:11:52 -0600

    hello world test

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -16,7 +16,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to the MITA State Self-Assessment Tool
+          HELLO WORLD
         </h1>
 
         <p className={styles.description}>
